8c23217167cc7a435a77b4739c6dc118553e5d7a
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2007-03-19 04:17:42 +0000

    getting closer...
    git-svn-id: http://billmill.org/svn/personal_code@4 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- python/svnvideo/svnvideo.py
+++ python/svnvideo/svnvideo.py
@@ -1,36 +1,43 @@
 from pysvn import Client, ClientError, Revision, opt_revision_kind
 import Image
+from difflib import Differ
 
 def getrev(client, url, rev):
-    diff = None
     try:
-        diff = client.diff('/tmp/', url, 
-            revision1=Revision(opt_revision_kind.number, rev),
-            revision2=Revision(opt_revision_kind.number, rev+1))
+        return client.cat(url, revision=Revision(opt_revision_kind.number, rev))
     except ClientError:
         print "Error grabbing revision %s of %s" % (rev, url)
-    return diff
+        return None
 
 def genimage(diff):
+    #TODO
     pass
+
+def groupdiffs(diffs):
+    #TODO
+    return []
 
 def applydiff(text, diff):
     print text
     print diff
+    raw_input("<enter> to continue")
 
 def makemovie(url, size):
     client = Client()
     images = []
     r1 = ""
+    d = Differ()
     maxrev = client.info2(url)[0][1]['rev'].number
 
     for rev in range(1, maxrev):
-        diff = getrev(client, url, rev)
-        if diff:
-            r2 = applydiff(r1, diff)
-            images.append(genimage(r2))
+        r2 = getrev(client, url, rev)
+        if r2:
+            diffs = list(d.compare(r1.splitlines(), r2.splitlines()))
+            print diffs
+            for group in groupdiffs(diffs):
+                images.append(genimage(r2))
             r1 = r2
 
 if __name__=="__main__":
-    testfile = "http://billmill.org/svn/buffet/trunk/buffet.py"
+    testfile = "http://billmill.org/svn/personal_code/python/svnvideo/svnvideo.py"
     makemovie(testfile, (300, 300))
