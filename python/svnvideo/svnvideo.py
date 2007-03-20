92a14718bec4ea3712a5da55997ce1f24b22da51
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2007-03-20 03:58:52 +0000

    Starting to write image generation code
    git-svn-id: http://billmill.org/svn/personal_code@7 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- python/svnvideo/svnvideo.py
+++ python/svnvideo/svnvideo.py
@@ -1,16 +1,30 @@
 from pysvn import Client, ClientError, Revision, opt_revision_kind
 import Image
-from difflib import Differ
+from ImageDraw import Draw
 
 def getrev(client, url, rev):
     try:
-        return client.cat(url, revision=Revision(opt_revision_kind.number, rev))
-    except ClientError:
-        print "Error grabbing revision %s of %s" % (rev, url)
-        return None
+        print "getting rev %s" % rev
+        catout = client.cat(url, revision=Revision(opt_revision_kind.number, rev))
+        diff = None
+        try:
+            diff = client.diff('/tmp/', url,
+                revision1=Revision(opt_revision_kind.number, rev-1),
+                revision2=Revision(opt_revision_kind.number, rev))
+        except ClientError, e:
+            print "Couldn't diff versions %s and %s of %s" % (rev, rev-1, url)
+        return (catout, diff)
+    except ClientError, e:
+        print "Error grabbing revision %s of %s: %s" % (rev, url, e)
+        return (None, None)
 
-def genimage(diff):
-    #TODO
+def gen_one(text, size):
+    i = Image.new("RGB", size, (255, 255, 255))
+    d = Draw(i)
+    d.text((0, 0), text)
+    i.show()
+
+def gen_diff(r1, r2, size):
     pass
 
 def groupdiffs(diffs):
@@ -18,39 +32,27 @@ def groupdiffs(diffs):
     storage = []
     for line in diffs[1:]:
         new_state = line[0:2]
-        #we don't even want to deal with 
-        if new_state == "  ": continue
         if new_state == state: storage.append(line)
         else:
             yield storage
             state = new_state
             storage = [line]
 
-def applydiff(text, diff):
-    print text
-    print diff
-    raw_input("<enter> to continue")
-
 def makemovie(url, size):
     client = Client()
     images = []
     r1 = ""
-    d = Differ()
     maxrev = client.info2(url)[0][1]['rev'].number
 
     for rev in range(1, maxrev):
-        r2 = getrev(client, url, rev)
-        if r2:
-            diffs = list(d.compare(r1.splitlines(), r2.splitlines()))
-            diffs = [line for line in diffs if not line.startswith("? ")]
-            for group in groupdiffs(diffs):
-                #FIXME: if I want to do groups like this, I'll need to
-                #figure out how to apply them to the file
-                print group
-                raw_input("<enter> to continue")
-                images.append(genimage(r2))
-            r1 = r2
+        (r2, diff) = getrev(client, url, rev)
+        if r2 and not diff:
+            #really wish I had multiple dispatch here
+            images.append(gen_one(r2, size))
+        elif r2 and diff:
+            images.append(gen_diff(r1, r2, size))
 
 if __name__=="__main__":
     testfile = "http://billmill.org/svn/personal_code/python/svnvideo/svnvideo.py"
     makemovie(testfile, (300, 300))
+
