e2056eaf03df8eb0cf96d4927707906159d31c35
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2007-03-20 04:48:31 +0000

    Now we're getting somewhere... images render, need to incorporate proper diffing
    git-svn-id: http://billmill.org/svn/personal_code@8 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- python/svnvideo/svnvideo.py
+++ python/svnvideo/svnvideo.py
@@ -1,6 +1,7 @@
 from pysvn import Client, ClientError, Revision, opt_revision_kind
 import Image
 from ImageDraw import Draw
+import ImageFont
 
 def getrev(client, url, rev):
     try:
@@ -18,14 +19,32 @@ def getrev(client, url, rev):
         print "Error grabbing revision %s of %s: %s" % (rev, url, e)
         return (None, None)
 
+LMARGIN = 3
+FONT = ImageFont.truetype('ProFontWindows.ttf', 12)
+DELTA_Y = FONT.getsize('|')[1] + 3
+
 def gen_one(text, size):
     i = Image.new("RGB", size, (255, 255, 255))
     d = Draw(i)
-    d.text((0, 0), text)
+    y = 0
+    for line in text.splitlines():
+        d.text((LMARGIN, y), line, font=FONT, fill='black')
+        y += DELTA_Y
     i.show()
 
 def gen_diff(r1, r2, size):
-    pass
+    i = Image.new("RGB", size, (255, 255, 255))
+    d = Draw(i)
+    font = ImageFont.truetype('ProFontWindows.ttf', 12)
+    y = 0
+    import pdb; pdb.set_trace()
+    for l1, l2 in map(None, r1.splitlines(), r2.splitlines()):
+        if l1 != l2:
+            d.text((LMARGIN, y), l2, font=FONT, fill='green')
+        else:
+            d.text((LMARGIN, y), l2, font=FONT, fill='black')
+        y += DELTA_Y
+    i.show()
 
 def groupdiffs(diffs):
     state = diffs[0][0:2]
@@ -51,8 +70,9 @@ def makemovie(url, size):
             images.append(gen_one(r2, size))
         elif r2 and diff:
             images.append(gen_diff(r1, r2, size))
+        r1 = r2
+        raw_input('<enter> to continue')
 
 if __name__=="__main__":
     testfile = "http://billmill.org/svn/personal_code/python/svnvideo/svnvideo.py"
-    makemovie(testfile, (300, 300))
-
+    makemovie(testfile, (500, 800))
