789d259a6aed12da37fc6ecab7994437d9654cd9
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2007-04-10 19:52:55 +0000

    slightly faster
    git-svn-id: http://billmill.org/svn/personal_code@11 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- python/bitmap_spoj/bitmap.py
+++ python/bitmap_spoj/bitmap.py
@@ -2,28 +2,31 @@ import sys
 
 def parse(stdin):
     n_prob = int(stdin.readline().strip())
+    out = []
     for i in range(n_prob):
-        whites = set()
+        whites = {}
         rows, cols = map(int, stdin.readline().strip().split())
         x = 0
         y = 0
         for i in range(rows):
             line = stdin.readline()
             for c in line:
-                if c == "1": whites.add((x, i))
+                if c == "1": whites[(x, i)] = None
                 x += 1
             x = 0
-        closest(rows, cols, whites)
+        out.extend(closest(rows, cols, whites))
         stdin.readline()
+    print "".join(out)
 
 def closest(rows, cols, whites):
+    #.888 with this one (fastest so far)
     out = []
     for j in range(rows):
         for i in range(cols):
             if (i, j) in whites: out.append("0")
-            else: out.append(str(min(abs(w[0] - i) + abs(w[1] - j) for w in whites)))
+            else: out.append(str(min(abs(w[0] - i) + abs(w[1] - j) for w in whites.iterkeys())))
             out.append(" ")
         out.append("\n")
-    print "".join(out)
+    return out
 
 parse(sys.stdin)
