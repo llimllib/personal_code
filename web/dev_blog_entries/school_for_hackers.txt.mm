3b29648f5397b5814947c784416804e63ddd0ddc
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2007-08-20 02:45:13 +0000

    Adding school for hackers and its mindmap
    git-svn-id: http://billmill.org/svn/personal_code@46 c4aad215-931b-0410-ba74-c4b90e0b6ad6

---
+++ web/dev_blog_entries/school_for_hackers.txt.mm
@@ -0,0 +1,36 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1187409805792" ID="Freemind_Link_1045331443" MODIFIED="1187409837455" TEXT="School for hackers">
+<node CREATED="1187409897028" ID="_" MODIFIED="1187409907282" POSITION="right" TEXT="flexible">
+<node CREATED="1187409940287" ID="Freemind_Link_1702557270" MODIFIED="1187409954595" TEXT="Classes have variable start/stop dates"/>
+<node CREATED="1187409955512" ID="Freemind_Link_1877185216" MODIFIED="1187409990808" TEXT="students don&apos;t all learn at the same pace"/>
+</node>
+<node CREATED="1187409907974" ID="Freemind_Link_178982567" MODIFIED="1187409914082" POSITION="right" TEXT="Open">
+<node CREATED="1187409996021" ID="Freemind_Link_1541963697" MODIFIED="1187410021369" TEXT="All source available (classes def - should students?)"/>
+<node CREATED="1187410022342" ID="Freemind_Link_113198305" MODIFIED="1187410032098" TEXT="Open source software whenever possible"/>
+<node CREATED="1187410032734" ID="Freemind_Link_1510977275" MODIFIED="1187410037082" TEXT="Contribute tools to open source"/>
+</node>
+<node CREATED="1187409915079" ID="Freemind_Link_1719358527" MODIFIED="1187409921354" POSITION="right" TEXT="Student Focus">
+<node CREATED="1187410047063" ID="Freemind_Link_572555022" MODIFIED="1187410051810" TEXT="No TAs"/>
+<node CREATED="1187410052647" ID="Freemind_Link_72409844" MODIFIED="1187410083709" TEXT="Professors have no research requirements">
+<node CREATED="1187410084376" ID="Freemind_Link_1819995508" MODIFIED="1187410094444" TEXT="Though they&apos;re encouraged to do work while there!"/>
+<node CREATED="1187410097312" ID="Freemind_Link_460759055" MODIFIED="1187410102116" TEXT="no tenure!"/>
+</node>
+<node CREATED="1187410111777" ID="Freemind_Link_779096099" MODIFIED="1187410128944" TEXT="Well-designed living environment (not designed for brochures"/>
+</node>
+<node CREATED="1187410131001" ID="Freemind_Link_1901257448" MODIFIED="1187410137285" POSITION="right" TEXT="Entrepenuership">
+<node CREATED="1187410138297" ID="Freemind_Link_571923373" MODIFIED="1187410153798" TEXT="Each school should run a business at which the students can work"/>
+<node CREATED="1187410182368" ID="Freemind_Link_927490170" MODIFIED="1187410189763" TEXT="Lectures from successful businessmen"/>
+<node CREATED="1187410197104" ID="Freemind_Link_1908926686" MODIFIED="1187410204084" TEXT="Assistance with starting own businesses"/>
+</node>
+<node CREATED="1187410217112" ID="Freemind_Link_949345355" MODIFIED="1187410227693" POSITION="right" TEXT="Practice">
+<node CREATED="1187410233785" ID="Freemind_Link_1397241474" MODIFIED="1187410250125" TEXT="Always be making!"/>
+<node CREATED="1187410250818" ID="Freemind_Link_482671738" MODIFIED="1187410313968" TEXT="Some &quot;classes&quot; may consist solely of projects">
+<node CREATED="1187410314468" ID="Freemind_Link_1768761092" MODIFIED="1187410350985" TEXT="Develop a new feature for an open-source program"/>
+</node>
+<node CREATED="1187410354485" ID="Freemind_Link_668265069" MODIFIED="1187410416419" TEXT="">
+<node CREATED="1187410359933" ID="Freemind_Link_1707012795" MODIFIED="1187410373265" TEXT="Teach programming as an art"/>
+</node>
+</node>
+</node>
+</map>
