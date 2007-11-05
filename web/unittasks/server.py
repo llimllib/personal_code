21bf97725335ffca21173ab2ee1eb695a740df5d
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2007-11-05 05:12:07 +0000

    just playing around some more
    git-svn-id: http://billmill.org/svn/personal_code@67 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- web/unittasks/server.py
+++ web/unittasks/server.py
@@ -2,33 +2,61 @@
 import os
 import cherrypy as cpy
 
+from md5 import md5
 from datetime import datetime
-from simplejson import dumps as json
-from couchdb import Server
+from couchdb import Server, ResourceConflict
 from couchdb.schema import Document, TextField, DateTimeField
 
 class User(Document):
-    username = TextField()
+    _id      = TextField()
     password = TextField()
-    type     = 'User'
+    type     = TextField(default='User')
     added    = DateTimeField(default=datetime.now)
 
-class aTunes:
+class Unittask:
     @cpy.expose
     def index(self, *args, **kwargs):
         return file("static/test.html")
 
+    def hash(self, password):
+        return md5(password).hexdigest()
+
     @cpy.expose
     def adduser(self, user, pw):
-        db = Server("http://localhost:8888/")['unit_tests']
-        User(username=user, password=pw)
+        db = Server("http://localhost:8888/")['unit_tasks']
+        try:
+            User(_id=user, password=self.hash(pw)).store(db)
+            return "added user"
+        except ResourceConflict:
+            return "user already exists"
 
     @cpy.expose
     def isuserp(self, user, pw):
-        db = Server("http://localhost:8888/")['unit_tests']
-        if len(db.view('isuserp', key={"username": user, "password": pw})):
-            return 1
-        return 0
+        db = Server("http://localhost:8888/")['unit_tasks']
+        if len(list(db.view('_design/users/isvalid', 
+                            key={"username": user, 
+                                 "password": self.hash(pw)}))):
+            return "Success!"
+        return "Failure :("
+
+    @cpy.expose
+    def deleteme(self):
+        db = Server("http://localhost:8888/")['unit_tasks']
+        #I'm thinking about this all wrong. You don't pass data into
+        # a query; it's like an index, you just pass it the key that you're
+        #trying to collect. I'm not quite sure how you set this up to, for
+        #example, validate a user, but I've got an idea that this is where I'm
+        #getting hung up.
+        res = db.query("""
+            function(doc) { 
+                if (doc.type == 'User')
+                    map(null, doc); 
+            }""")
+        template = "<html><body>count: %s<br>list: %s</body></html>"
+        rows = list(res)
+        rowss = str(rows)
+        return template % (len(rows), rowss.replace('<', "&lt;"))
+
 
     @cpy.expose
     def newTaskAjax(self, taskName, frequency):
@@ -40,12 +68,12 @@ def create_database():
 
     #XXX: figure out how to run python view functions?
     db.create({
-        "id":    "_design/isuserp",
+        "_id":    "_design/users",
         "views": {
-            "isuserp": """
+            "isvalid": """
                 function(doc) {
                     if (doc.Type == 'User' &&
-                        key.username == doc.username &&
+                        key.username == doc._id &&
                         key.password == doc.password)
                     {
                         map(null, doc);
@@ -54,7 +82,27 @@ def create_database():
         }
     })
 
+    User(_id="llimllib", password=md5("tao").hexdigest()).store(db)
+
+def debug():
+    db = Server('http://localhost:8888/')['unit_tasks']
+
+    f = db["_design/users"]
+    f["views"] = {
+            "isvalid": """
+                function(doc) {
+                    if (doc.type == 'User')
+                    {
+                        map(null, doc);
+                    }
+                }"""
+        }
+    db["_design/users"] = f
+
+    #User(_id="llimllib", password=md5("tao").hexdigest()).store(db)
+
 if __name__ == "__main__":
+    debug()
     if 'unit_tasks' not in Server('http://localhost:8888/'):
         create_database()
     pwd = os.path.dirname(os.path.abspath(__file__))
@@ -62,4 +110,4 @@ if __name__ == "__main__":
                            'tools.staticdir.dir': os.path.join(pwd, "static")
                           },
            }
-    cpy.quickstart(aTunes(), '/', config=conf)
+    cpy.quickstart(Unittask(), '/', config=conf)
