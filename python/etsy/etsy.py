7be78949a9b7b7aff31aaa24cba80c517704c1c4
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2007-12-13 05:38:57 +0000

    added a docstring and removed unnecessary print
    git-svn-id: http://billmill.org/svn/personal_code@73 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- python/etsy/etsy.py
+++ python/etsy/etsy.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+"""
+Code for etsy job application. Requires python 2.5 (or ElementTree
+http://effbot.org/zone/element-index.htm)
+
+Simply execute this file to count the number of users in each city and print 
+out a running total.
+"""
+
 from urllib import urlopen
 from xml.etree.ElementTree import iterparse
 
@@ -10,5 +19,4 @@ for doc in docs:
     for event, elem in iterparse(doc):
         if elem.tag == "city":
             totals[elem.text] = totals.get(elem.text, 0) + 1
-            print "found " + elem.tag
             print totals
