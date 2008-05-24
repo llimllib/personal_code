e8806d34547f5453bd80faa69ddfe6ed1b7b7732
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2008-05-24 04:36:20 +0000

    Able to parse the entire cmap section
    git-svn-id: http://billmill.org/svn/personal_code@179 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- python/ttf_parse/parse.py
+++ python/ttf_parse/parse.py
@@ -63,17 +63,120 @@ print table_directory
 #    subtracting one made the two true vales negative by a small amount, but 
 #    didn't affect many of the false values and the ones it did affect are
 #    still *way* too large
-for key in table_directory:
-    try:
-        checksum, offset, length = table_directory[key]
-        nlongs = (length + 3) / 4 #will fail in py3k
-        mysum = sum(unpack(">%sI" % nlongs, buf[offset:offset+(nlongs*4)]))
-        print key, checksum, mysum, checksum == mysum or mysum - checksum
-        i = 0
-        while mysum > checksum:
-            nlongs -= 1
-            mysum = sum(unpack(">%sI" % nlongs, buf[offset:offset+(nlongs*4)]))
-            i += 1
-        if i: print "it took %s tries to get to %s" % (i, mysum)
-    except Exception, e:
-        print str(e) + ", got %s" % length
+#for key in table_directory:
+#    try:
+#        checksum, offset, length = table_directory[key]
+#        nlongs = (length + 3) / 4 #will fail in py3k
+#        mysum = sum(unpack(">%sI" % nlongs, buf[offset:offset+(nlongs*4)]))
+#        print key, checksum, mysum, checksum == mysum or mysum - checksum
+#        i = 0
+#        #while mysum > checksum:
+#        #    nlongs -= 1
+#        #    mysum = sum(unpack(">%sI" % nlongs, buf[offset:offset+(nlongs*4)]))
+#        #    i += 1
+#        #if i: print "it took %s tries to get to %s" % (i, mysum)
+#    except Exception, e:
+#        print str(e) + ", got %s" % length
+
+#hmmm.... well that didn't work well. We got correct checksums for only two tables.
+#let's try and read some data from the cmap table and see if it gives any hints:
+#UInt16  	version         	Version number (Set to zero)
+#UInt16 	numberSubtables 	Number of encoding subtables
+ptr = table_directory['cmap'][1]
+header = ">2H"
+version, numberSubtables = unpack(header, buf[ptr:ptr+calcsize(header)])
+print "version: %s subtables: %s" % (version, numberSubtables)
+ptr += calcsize(header)
+
+#next up come the "encoding subtables"
+#UInt16  	platformID          Platform identifier
+#UInt16 	platformSpecificID 	Platform-specific encoding identifier
+#UInt32 	offset 	            Offset of the mapping table
+header = ">HHI"
+sz = calcsize(header)
+subtables = [unpack(header, buf[ptr+sz*i:ptr+sz*(i+1)])
+             for i
+             in range(numberSubtables)]
+print subtables
+
+#next we read a short to determine what format we've got:
+ptr = table_directory['cmap'][1]
+#the first font table is a type 0:
+#UInt16  	format  	Set to 0
+#UInt16 	length 	Length in bytes of the subtable (set to 262 for format 0)
+#UInt16 	language 	Language code for this encoding subtable, or zero if language-independent
+#UInt8 	glyphIndexArray[256] 	An array that maps character codes to glyph index values
+
+#the second is type 4:
+#UInt16  	format  	Format number is set to 4  	 
+#UInt16 	length 	Length of subtable in bytes 	 
+#UInt16 	language 	Language code for this encoding subtable, or zero if language-independent 	 
+#UInt16 	segCountX2 	2 * segCount 	 
+#UInt16 	searchRange 	2 * (2**FLOOR(log2(segCount))) 	 
+#UInt16 	entrySelector 	log2(searchRange/2) 	 
+#UInt16 	rangeShift 	(2 * segCount) - searchRange 	 
+#UInt16 	endCode[segCount] 	Ending character code for each segment, last = 0xFFFF. 	
+#UInt16 	reservedPad 	This value should be zero 	
+#UInt16 	startCode[segCount] 	Starting character code for each segment 	
+#UInt16 	idDelta[segCount] 	Delta for all character codes in segment 	 
+#UInt16 	idRangeOffset[segCount] 	Offset in bytes to glyph indexArray, or 0 	 
+#UInt16 	glyphIndexArray[variable] 	Glyph index array
+#for now we'll just load those first 7 variables to see what's going on
+for i, table in enumerate(subtables):
+    _, _, offset = table
+    start = ptr + offset * i
+    #sometimes format is not a short. We'll ignore these cases.
+    format = unpack(">H", buf[start:start+2])[0]
+    if format == 0:
+        fmt = ">3H256B"
+        data = unpack(fmt, buf[start:start+calcsize(fmt)])
+        print "table %s type 0:\n%s" % (i, data)
+    elif format == 4:
+        fmt = ">7H"
+        sz = calcsize(fmt)
+        data = unpack(fmt, buf[start:start + sz])
+        cur = start + sz
+
+        segCount = data[3] / 2
+
+        fmt = ">%sH" % segCount
+        sz = calcsize(fmt)
+        endCode = unpack(fmt, buf[cur:cur + sz])
+        cur = cur + sz
+        assert endCode[-1] == 0xFFFF
+
+        assert unpack(">H", buf[cur:cur+2]) == (0,)
+        cur += 2
+
+        fmt = ">%sH" % segCount
+        sz = calcsize(fmt)
+        startCode = unpack(fmt, buf[cur:cur + sz])
+        cur = cur + sz
+
+        fmt = ">%sH" % segCount
+        sz = calcsize(fmt)
+        idDelta = unpack(fmt, buf[cur:cur + sz])
+        cur = cur + sz
+
+        fmt = ">%sH" % segCount
+        sz = calcsize(fmt)
+        idRangeOffset = unpack(fmt, buf[cur:cur + sz])
+        cur = cur + sz
+
+        #To use these arrays, it is necessary to search for the first endCode that is
+        #greater than or equal to the character code to be mapped. If the corresponding
+        #startCode is less than or equal to the character code, then use the
+        #corresponding idDelta and idRangeOffset to map the character code to the glyph
+        #index. Otherwise, the missing character glyph is returned. To ensure that the
+        #search will terminate, the final endCode value must be 0xFFFF. This segment
+        #need not contain any valid mappings. It can simply map the single character
+        #code 0xFFFF to the missing character glyph, glyph 0.  
+        assert len(endCode) == len(startCode) == len(idDelta) == len(idRangeOffset) 
+        data += (tuple(zip(startCode, endCode, idDelta, idRangeOffset)),) 
+        for char in data[7]:
+            #start must be <= end; I don't understand why they can = 
+            assert char[0] <= char[1] 
+
+        print "table %s type 4:\n%s" % (i, data) 
+    else: print "unable to read format %s of table %s" % (format, i)
+
