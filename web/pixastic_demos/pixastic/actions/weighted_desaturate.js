781f7af8e7c8b4dc50008c135368a092b1e025a7
Bill Mill <bill.mill@gmail.com>
2009-03-03 01:25:37 -0500

    editing editing editing

--- web/pixastic_demos/pixastic/actions/weighted_desaturate.js
+++ web/pixastic_demos/pixastic/actions/weighted_desaturate.js
@@ -7,7 +7,7 @@ Pixastic.Actions.weighted_desaturate = {
       var x = w;
       do {
         var offset = offsetY + (x-1)*4;
-        newval = visit(data[offset], data[offset+1], data[offset+2], data[offset+3]);
+        var newval = visit(data[offset], data[offset+1], data[offset+2], data[offset+3]);
         data[offset]   = newval[0] || data[offset];
         data[offset+1] = newval[1] || data[offset+1];
         data[offset+2] = newval[2] || data[offset+2];
@@ -16,26 +16,26 @@ Pixastic.Actions.weighted_desaturate = {
     } while (--y);
   },
 
-	process : function(params) {
+  process : function(params) {
     var rweight = params.options.r;
     var gweight = params.options.g;
     var bweight = params.options.b;
-
+    
     var scale = 1 / (rweight + gweight + bweight);
     rweight *= scale;
     gweight *= scale;
     bweight *= scale;
-
-		if (Pixastic.Client.hasCanvasImageData()) {
-			var data = Pixastic.prepareData(params);
-			var rect = params.options.rect;
-			var w = rect.width;
-			var h = rect.height;
+    
+    if (Pixastic.Client.hasCanvasImageData()) {
+      var data = Pixastic.prepareData(params);
+      var rect = params.options.rect;
+      var w = rect.width;
+      var h = rect.height;
       this.each_pixel(data, w, h, function(r, g, b, a) {
-        brightness = r * rweight + g * gweight + b * bweight;
+        var brightness = r * rweight + g * gweight + b * bweight;
         return [brightness, brightness, brightness];
       });
-			return true;
+      return true;
     }
   },
 
