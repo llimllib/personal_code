97ffc1c0d07e193776a94180a56d232502ed7f59
Bill Mill <bill.mill@gmail.com>
2009-03-16 00:40:50 -0400

    checking in current bracket code... needs a rewrite into mako

--- python/kenpom_brackets/bracket.py
+++ python/kenpom_brackets/bracket.py
@@ -9,9 +9,45 @@ kenpom = [x.groups() for x in [re.search(teamre, line) for line in file("kenpom.
 #(rank, name, record, pythagorean score, adjusted O, adj O rank, adjusted D, adj D rank)
 kenpom = dict((x[1], x) for x in kenpom)
 
+#make sure we agree on all our team names
 for r in bracket:
   for t in bracket[r].values():
     if type(t) == type([]):
       for t2 in t:
-        if t2 not in kenpom: print t2
-    elif t not in kenpom: print t
+        assert(t2 in kenpom)
+    else:
+      assert(t in kenpom)
+
+#next task: find a simple way to print out a bracket:
+out = file("out.html", "w")
+out.write("""<html>
+<head><style>
+.top { border-style: none none solid none; }
+.bottom { border-style: none solid solid none; }
+tr { padding-bottom: 10px; }
+</style>
+<body><table cellspacing=0>
+""")
+for region in ("Midwest", "West", "East", "South"):
+  out.write("<tr><td colspan=11><h1>%s</h1></td></tr>" % region)
+  for seed in (1,8,5,4,6,3,7,2):
+    t = bracket[region][seed]
+    oppseed = 17-seed
+    t2 = bracket[region][oppseed]
+    if type(t2) == type([]):
+      rank, name, record, pyth, adjo, adjorank, adjd, adjdrank = kenpom[t]
+      out.write('<tr><td></td><td class="top">1 %s (%s, %s, %s)</td></tr>\n' % (t, pyth, adjo, adjd))
+      tx, ty = t2
+      rank, name, record, pyth, adjo, adjorank, adjd, adjdrank = kenpom[tx]
+      out.write('<tr><td class="top">%s (%s, %s, %s)</td><td class="bottom"></tr>\n' % (tx, pyth, adjo, adjd))
+      rank, name, record, pyth, adjo, adjorank, adjd, adjdrank = kenpom[ty]
+      out.write('<tr><td class="bottom">%s (%s, %s, %s)</td><td></td></tr>\n' % (ty, pyth, adjo, adjd))
+      out.write("</tr>\n")
+    else:
+      out.write("<tr><td></td>")
+      rank, name, record, pyth, adjo, adjorank, adjd, adjdrank = kenpom[t]
+      out.write('<td class="top">%s %s (%s, %s, %s)</td></tr>\n' % (seed, t, pyth, adjo, adjd))
+      rank, name, record, pyth, adjo, adjorank, adjd, adjdrank = kenpom[t2]
+      out.write('<tr><td></td><td class="bottom">%s %s (%s, %s, %s)</td></tr>\n' % (oppseed, t2, pyth, adjo, adjd))
+out.write("</table></html>")
+out.close()
