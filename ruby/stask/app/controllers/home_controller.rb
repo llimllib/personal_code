3130a6514e8807fddb2cea2d0f1bc3fe2bb698ae
bill <bill.mill@gmail.com>
2010-07-26 16:35:32 -0400

    just working through a bit more refresher

--- ruby/stask/app/controllers/home_controller.rb
+++ ruby/stask/app/controllers/home_controller.rb
@@ -1,5 +1,28 @@
 class HomeController < ApplicationController
   def index
+    @task = Task.new
+    @tasks = Task.all
+
+    respond_to do |format|
+      format.html
+      format.xml  { render :xml => @posts }
+    end
   end
 
+  def create
+    @task = Task.new(params[:task])
+
+    respond_to do |format|
+      if @task.save
+        flash[:notice] = 'Task was successfully created.'
+        format.html { redirect_to(index) }
+        format.xml  { render :xml => @task, :status => :created,
+                    :location => @task }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @task.errors,
+                    :status => :unprocessable_entity }
+      end
+    end
+  end
 end
