40642cdb049255ffc90843f69b521c0d4ea77247
bill <bill.mill@gmail.com>
2010-12-06 11:25:44 -0500

    checking in my crossword code, I don't even remember where I was on this

--- python/reddit_ai_101/lecture_4/xwordspec.py
+++ python/reddit_ai_101/lecture_4/xwordspec.py
@@ -23,33 +23,50 @@ class Xword:
 
     def setspec(self, spec):
         self.spec = spec
+        #XXX: use numpy for speed, and to get columns more easily?
         self.mtx = [list(x) for x in spec.split()]
+        self.crosses = [[[] for y in x] for x in spec.split()]
         #an array of ((x1,y1),(x2,y2)) word definitions
         self.words = self.getwords()
 
     def getwords(self):
+        def find(arr, val, start):
+            try:
+                idx = arr.index(val, start)
+            except:
+                idx = len(arr)
+            return idx
+
         rowlen = len(self.mtx)
         cols = list(zip(*self.mtx))
         words = []
+        wordn = 0
         for i, row in enumerate(self.mtx):
             for j, letter in enumerate(self.mtx[i]):
                 if letter != "#":
                     if j < len(row)-1 and self.mtx[i][j+1] != '#' \
                     and (j==0 or self.mtx[i][j-1] == '#'):
                         #we have an across word, find the end and add it to words
-                        try:
-                            end = row.index('#', j)
-                        except ValueError:
-                            end = len(row)
+                        end = find(row, '#', j)
                         words.append(((j,i),(end-1,i)))
+                        wordn += 1
+                        self.mark_crosses(wordn, ((j,i),(end-1,i)))
                     if i < rowlen-1 and self.mtx[i+1][j] != '#' \
                     and (i==0 or self.mtx[i-1][j] == '#'):
-                        try:
-                            end = cols[j].index('#', i)
-                        except ValueError:
-                            end = len(cols[j])
+                        end = find(cols[j], '#', i)
                         words.append(((j,i),(j,end-1)))
+                        wordn += 1
+                        self.mark_crosses(wordn, ((j,i),(j,end-1)))
         return words
+
+    def mark_crosses(wordn, word):
+        (x1, y1), (x2, y2) = word
+        if x1 == x2:
+            for i in range(y1, y2+1):
+                self.crosses[i][x1].append(wordn)
+        else:
+            for i in range(x1, x2+1):
+                self.crosses[y1][i].append(wordn)
 
     def getspec(self):
         return "\n".join("".join(r) for r in self.mtx)
@@ -62,6 +79,7 @@ class Xword:
         else:
             return "".join(self.mtx[y1][x1:x2+1])
 
+    #insert string @s at @word
     def insert(self, word, s):
         (x1, y1), (x2, y2) = word
         letters = list(s)
@@ -79,6 +97,12 @@ class Xword:
             self.nextword += 1
         return self.words[self.nextword]
 
+    def isvalid(self, wordlist):
+        for word in self.words:
+            if self.getword(word) not in wordlist:
+                return false
+        return true
+
     def copy(self):
         x = Xword()
         x.spec = self.spec
@@ -90,21 +114,25 @@ class Xword:
     def complete(self):
         return "".join("".join(x) for x in self.mtx).find('.') == -1
 
+    def fits(self, s):
+        word = self.xword.getnextword()
+        if re.match(self.xword.getword(self.xword.getnextword()), word):
+            return True
+        return False
+
     def __str__(self):
         return "\n".join("".join(x) for x in self.mtx) + "\n--------------"
 
 class Node:
-    def __init__(self, word, xword):
+    def __init__(self, word, xword, wordlist):
         self.xword = xword
         self.word = word
         self.xword.insert(self.xword.getnextword(), word)
+        self.wordlist = wordlist
 
     def complete(self): return self.xword.complete()
 
-    def fits(self, word):
-        if re.match(self.xword.getword(self.xword.getnextword()), word):
-            return True
-        return False
+    def fits(self, word): return self.xword.fits(word)
 
     def __str__(self):
         return self.word + ":\n\n" + str(self.xword)
@@ -113,25 +141,30 @@ def makegraph(words, spec):
     for word in words:
         xword = Xword()
         xword.setspec(spec)
-        root = Node(word, xword)
+        root = Node(word, xword, words)
         _makegraph(words, root)
 
 def _makegraph(words, root):
     for word in words:
         if root.fits(word):
-            n = Node(word, root.xword.copy())
+            n = Node(word, root.xword.copy(), words)
             if n.complete():
                 print(n)
             else:
                 _makegraph(words, n)
+        #we no longer need node n, free that memory
+        del n
         
 if __name__=="__main__":
     x = Xword()
-    x.setspec("""...
+    spec = """...
 ...
-...""")
+..."""
+    x.setspec(spec)
     print(x.words)
     print(x.getspec())
+
+    makegraph(getwords("words.txt"), spec)
 
     x = Xword()
     spec = """...#...
@@ -141,4 +174,3 @@ if __name__=="__main__":
     print(x.words)
     print(x.getspec())
 
-    makegraph(getwords("words.txt"), spec)
