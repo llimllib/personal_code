bf43a15fe79a4143a9408e349c936dd33cc5626d
Bill Mill <bill@billmill.org>
2017-12-15 10:54:52 -0500

    improved C version

--- misc/advent/2017/15/d.c
+++ misc/advent/2017/15/d.c
@@ -1,18 +1,14 @@
 /* clang d.c -O3 -o d */
-/* This is .3s, pypy is only .42! */
+/* This is .12s, pypy is only .42! */
 #include<stdio.h>
 
 int go(long a, long b, long iters) {
     int judge = 0;
     for (int i=0; i < iters; i++) {
-        while(1) {
-            a = a * 16807 % 2147483647;
-            if (a % 4 == 0) break;
-        }
-        while(1) {
-            b = b * 48271 % 2147483647;
-            if (b % 8 == 0) break;
-        }
+        /* % -> & due to askalski:
+         * https://www.reddit.com/r/adventofcode/comments/7jxkiw/2017_day_15_solutions/dra1dep/ */
+        do a = a * 16807 % 2147483647; while (a & 4);
+        do b = b * 48271 % 2147483647; while (b & 8);
         if ((a & 0xFFFF) == (b & 0xFFFF))
             judge += 1;
     }
