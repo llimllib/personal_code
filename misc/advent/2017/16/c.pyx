e96b14b0d05a110c4a415df4033ecc4759c28b62
Bill Mill <bill@billmill.org>
2017-12-18 12:04:48 -0500

    add failed cython answer for 16

--- misc/advent/2017/16/c.pyx
+++ misc/advent/2017/16/c.pyx
@@ -12,22 +12,33 @@ def instrs(inp):
             instructions.append(("s", n1, n2))
     return instructions
 
-def go(inp, cycles=1):
-    cdef int progs[16]
-    progs = list(range(n))
+def go(inp, int cycles=1):
+    cdef char *progs = "abcdefghijklmnop"
+    cdef char temp[16] = "abcdefghijklmnop"
     cdef int n = 16
     cdef int d, a, b, n1, n2, i1, i2, i, j
-    instructions = instrs()
+    instructions = instrs(inp)
     for i in range(cycles):
         for inst in instructions:
+            print(inst, progs, temp)
             if inst[0] == "s":
-                d = int(inst[1:])
-                for j in range(n-d, 16):
-                    progs[(n-d)+j] = progs[j]
+                d = inst[1]
+                for k in range(16):
+                    temp[k] = progs[(16-d+k) % 16]
+                for k in range(16):
+                    progs[k] = temp[k]
             elif inst[0] == "x":
-                progs[inst[1]], progs[inst[2]] = progs[inst[2]], progs[inst[1]]
+                print("a", inst, progs, temp)
+                temp[0] = progs[inst[1]]
+                print("b", inst, progs, temp)
+                progs[inst[1]] = progs[inst[2]]
+                print("c", inst, progs, temp)
+                progs[inst[2]] = temp[0]
             else:
                 i1 = progs.index(inst[1])
                 i2 = progs.index(inst[2])
                 progs[i1], progs[i2] = progs[i2], progs[i1]
     return ''.join(chr(p + 97) for p in progs)
+
+def main():
+    print(go(open("input.txt").read()))
