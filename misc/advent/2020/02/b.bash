288e30f141c1242f4039d2cd6ea079cfd2903eb4
Bill Mill <bill@billmill.org>
2020-12-02 16:24:42 -0500

    add bash solution

---
+++ misc/advent/2020/02/b.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+i=0
+j=0
+while read -r -a line; do
+    ltr=${line[1]//:/}
+
+    # sed outputs a newline and I couldn't figure out how to suppress it, so subract one
+    ltrcount=$(($(echo "${line[2]}" | sed "s/[^${ltr}]//g" | wc -c)-1))
+
+    min=${line[0]//-*/}
+    max=${line[0]//*-/}
+    # echo "$ltr $min $max $ltrcount"
+    if [[ $min -le $ltrcount ]] && [[ $max -ge $ltrcount ]]; then
+        i=$((i+1))
+    fi
+
+    a=$([[ ${ltr} == "${line[2]:$((min-1)):1}" ]] && echo "true" || echo "false")
+    b=$([[ ${ltr} == "${line[2]:$((max-1)):1}" ]] && echo "true" || echo "false")
+    if [[ "$a" != "$b" ]]; then
+        j=$((j+1))
+    fi
+done < input.txt
+echo $i
+echo $j
