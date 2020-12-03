69124f7180598b4a0b6bfd0af7ccfca8883ae672
Bill Mill <bill@billmill.org>
2020-12-03 00:11:41 -0500

    1440/1099, not too bad

---
+++ misc/advent/2020/03/a.py
@@ -0,0 +1,23 @@
+map_ = [list(l) for l in open("input.txt").read().split("\n") if l]
+w = len(map_[0])
+
+
+def search(xinc, yinc):
+    x, y = (0, 0)
+    trees = 0
+    while 1:
+        if y >= len(map_):
+            break
+        try:
+            if map_[y][x] == "#":
+                trees += 1
+        except IndexError:
+            print(y, x)
+            break
+        x = (x + xinc) % w
+        y = y + yinc
+    return trees
+
+
+print(search(3, 1))
+print(search(3, 1) * search(1, 1) * search(5, 1) * search(7, 1) * search(1, 2))
