1901ea5e1a75e9efc824ca002f79601dad80fed2
Bill Mill <bill@billmill.org>
2020-12-04 22:54:12 -0500

    wip

--- misc/advent/2020/04/a.zig
+++ misc/advent/2020/04/a.zig
@@ -9,59 +9,99 @@ const fs = std.fs;
 // ecl (Eye Color)
 // pid (Passport ID)
 // cid (Country ID)
-const Passport = struct {
-    byr: usize,
-    iyr: usize,
-    eyr: usize,
-    hgt: usize,
-    hgtUnit: []u8,
-    hcl: []u8,
-    ecl: []u8,
-    pid: []u8,
-    cid: []u8,
-};
+const PassportMap = std.StringHashMap([]const u8);
+const PassportList = std.ArrayList(PassportMap);
 
 // given an input buffer, return the number of valid passport entries
-fn valid1(input: []Passport) usize {}
-
-fn parse(allocator: *std.mem.Allocator, input: []u8) !std.ArrayList(Passport) {
-    var passports = std.ArrayList(Passport).init(allocator);
-    var lines = std.mem.split(input, "\n");
-    var passport = Passport{
-        .byr = 0,
-        .iyr = 0,
-        .eyr = 0,
-        .hgt = 0,
-        .hgtUnit = "",
-        .hcl = "",
-        .ecl = "",
-        .pid = "",
-        .cid = "",
-    };
-    while (lines.next()) |line| {
-        if (line.len == 0) {
-            try passports.append(passport);
-            passport = Passport{
-                .byr = 0,
-                .iyr = 0,
-                .eyr = 0,
-                .hgt = 0,
-                .hgtUnit = "",
-                .hcl = "",
-                .ecl = "",
-                .pid = "",
-                .cid = "",
-            };
-            continue;
-        }
-
-        var data = std.mem.tokenize(line, " :");
-        while (data.next()) |key| {
-            var val = data.next().?;
-            if (std.mem.eql(u8, key, "byr")) {
-                passport.byr = try std.fmt.parseInt(usize, val, 10);
+fn valid1(passports: PassportList) usize {
+    const required = &[_][]const u8{ "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+    var valid: usize = 0;
+    outer: for (passports.items) |passport| {
+        for (required) |key| {
+            if (passport.get(key) == null) {
+                continue :outer;
             }
         }
+        valid += 1;
+    }
+    return valid;
+}
+
+fn find(haystack: *const [7][]const u8, needle: []const u8) bool {
+    for (haystack) |elt| {
+        if (std.mem.eql(u8, elt, needle)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn valid2(passports: PassportList) !usize {
+    const required = &[_][]const u8{ "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+    const validEcl = &[_][]const u8{ "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+    var valid: usize = 0;
+    invalid: for (passports.items) |passport| {
+        for (required) |key| {
+            if (passport.get(key) == null) {
+                continue :invalid;
+            }
+        }
+        var byr = try std.fmt.parseInt(usize, passport.get("byr").?, 10);
+        if (byr < 1920 or byr > 2002) {
+            continue :invalid;
+        }
+        var iyr = try std.fmt.parseInt(usize, passport.get("iyr").?, 10);
+        if (iyr < 2010 or iyr > 2020) {
+            continue :invalid;
+        }
+        var eyr = try std.fmt.parseInt(usize, passport.get("eyr").?, 10);
+        if (eyr < 2020 or eyr > 2030) {
+            continue :invalid;
+        }
+
+        var parts = std.mem.tokenize(passport.get("hgt").?, "ci");
+        var h = try std.fmt.parseInt(usize, parts.next().?, 10);
+        var unit = parts.next();
+        if (unit == null) {
+            continue :invalid;
+        }
+        if ((std.mem.eql(u8, unit.?, "in") and (h < 59 or h > 76)) or (std.mem.eql(u8, unit.?, "cm") and (h < 150 or h > 193))) {
+            continue :invalid;
+        }
+
+        var hcl = passport.get("hcl").?;
+        if (hcl.len != 7 or hcl[0] != '#') {
+            continue :invalid;
+        }
+        var i: usize = 6;
+        while (i > 0) : (i -= 1) {
+            if (!std.ascii.isDigit(hcl[i]) and (hcl[i] < 'a' or hcl[i] > 'f')) {
+                std.debug.print("{} {}\n", .{ hcl[i], hcl });
+                continue :invalid;
+            }
+        }
+
+        if (!find(validEcl, passport.get("ecl").?)) {
+            continue :invalid;
+        }
+
+        valid += 1;
+    }
+    return valid;
+}
+
+fn parse(allocator: *std.mem.Allocator, input: []u8) !std.ArrayList(PassportMap) {
+    var passports = PassportList.init(allocator);
+    var passport_entries = std.mem.split(input, "\n\n");
+    var passport = PassportMap.init(allocator);
+    while (passport_entries.next()) |entry| {
+        var data = std.mem.tokenize(entry, " :\n");
+        while (data.next()) |key| {
+            var val = data.next().?;
+            try passport.put(key, val);
+        }
+        try passports.append(passport);
+        passport = PassportMap.init(allocator);
     }
 
     return passports;
@@ -73,5 +113,12 @@ pub fn main() !void {
     const input = try std.fs.cwd().readFileAlloc(&alloc.allocator, args[1], std.math.maxInt(usize));
 
     var passports = try parse(&alloc.allocator, input);
-    std.debug.print("{}\n", .{passports.items.len});
+    std.debug.print("total: {}\n", .{passports.items.len});
+    std.debug.print("valid part 1: {}\n", .{valid1(passports)});
+    std.debug.print("valid part 2: {}\n", .{valid2(passports)});
+    // XXX: function to print hash entry nicely
+    // var it = passports.items[0].iterator();
+    // while (it.next()) |key| {
+    //     std.debug.print("{}\n", .{key});
+    // }
 }
