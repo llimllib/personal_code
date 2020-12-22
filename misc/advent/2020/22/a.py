8df0d693d4b057aa491a100ab75da19d8376eaa9
Bill Mill <bill@billmill.org>
2020-12-22 09:28:53 -0500

    cleanup

--- misc/advent/2020/22/a.py
+++ misc/advent/2020/22/a.py
@@ -15,20 +15,19 @@ win = p1 if p1 else p2
 print(f"part 1: {sum(a*b for a,b in zip(reversed(win), range(1,len(win)+1)))}")
 
 
-def rc(p1, p2, game=1):
+def rc(p1, p2):
     history = set()
 
-    rnd = 0
     while p1 and p2:
         key = (tuple(p1), tuple(p2))
+        key = "".join(p1) + "|" + "".join(p2)
         if key in history:
             return 1
         history.add(key)
-        rnd += 1
         c1 = p1.pop(0)
         c2 = p2.pop(0)
         if len(p1) >= c1 and len(p2) >= c2:
-            result = rc(p1[:c1], p2[:c2], game + 1)
+            result = rc(p1[:c1], p2[:c2])
             p1.extend([c1, c2]) if result == 1 else p2.extend([c2, c1])
         else:
             p1.extend([c1, c2]) if c1 > c2 else p2.extend([c2, c1])
