6e2c78fc6279ef70128c3d362c701d24a327586e
Bill Mill <bill@billmill.org>
2022-12-01 10:26:49 -0500

    simpler

--- misc/advent/2022/01/a.py
+++ misc/advent/2022/01/a.py
@@ -1,18 +1,14 @@
 print(
     sorted(
-        [
-            sum(map(int, l.split("\n")))
-            for l in open("input.txt").read().strip().split("\n\n")
-        ]
+        sum(map(int, l.split("\n")))
+        for l in open("input.txt").read().strip().split("\n\n")
     )[-1]
 )
 print(
     sum(
         sorted(
-            [
-                sum(map(int, l.split("\n")))
-                for l in open("input.txt").read().strip().split("\n\n")
-            ]
+            sum(map(int, l.split("\n")))
+            for l in open("input.txt").read().strip().split("\n\n")
         )[-3:]
     )
 )
