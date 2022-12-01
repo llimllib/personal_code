b728dcd635165d61bd1d945f38ffc544ef8f00a4
Bill Mill <bill@billmill.org>
2022-12-01 09:21:29 -0500

    add a test

--- misc/advent/2022/01/a.zig
+++ misc/advent/2022/01/a.zig
@@ -1,13 +1,14 @@
 // run with: zig run a.zig
 const std = @import("std");
+const testing = std.testing;
 
-const data = @embedFile("input.txt");
+const input = @embedFile("input.txt");
 
 fn gt(_: void, lhs: usize, rhs: usize) bool {
     return lhs > rhs;
 }
 
-pub fn main() !void {
+pub fn calories(data: []const u8) ![3]usize {
     var elves = std.mem.split(u8, data, "\n\n");
 
     var maxes = [_]usize{ 0, 0, 0 };
@@ -23,6 +24,33 @@ pub fn main() !void {
         }
     }
 
+    return maxes;
+}
+
+pub fn main() !void {
+    var maxes = try calories(input);
+
     std.debug.print("part 1: {}\n", .{maxes[0]});
     std.debug.print("part 2: {}\n", .{maxes[0] + maxes[1] + maxes[2]});
+}
+
+// zig test a.zig
+test "sample" {
+    var result = try calories(
+        \\1000
+        \\2000
+        \\3000
+        \\
+        \\4000
+        \\
+        \\5000
+        \\6000
+        \\
+        \\7000
+        \\8000
+        \\9000
+        \\
+        \\10000
+    );
+    try testing.expect(std.mem.eql(usize, &result, &[3]usize{ 24000, 11000, 10000 }));
 }
