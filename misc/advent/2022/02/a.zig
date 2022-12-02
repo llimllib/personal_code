231910b19b9851b2b91958dce1ea4e92ec54895e
Bill Mill <bill@billmill.org>
2022-12-02 08:37:29 -0500

    part 2

--- misc/advent/2022/02/a.zig
+++ misc/advent/2022/02/a.zig
@@ -42,18 +42,71 @@ pub fn score(data: []const u8) !i32 {
     return totalScore;
 }
 
-pub fn main() !void {
-    var s = try score(input);
+const LOSE = 'X';
+const DRAW = 'Y';
+const WIN = 'Z';
 
-    std.debug.print("part 1: {}\n", .{s});
+pub fn choosePlay(opponent: i32, play: u8) i32 {
+    if (play == DRAW) {
+        return opponent;
+    } else if (play == LOSE) {
+        return switch (opponent) {
+            ROCK => SCISSORS,
+            PAPER => ROCK,
+            SCISSORS => PAPER,
+            else => unreachable,
+        };
+    } else {
+        return switch (opponent) {
+            ROCK => PAPER,
+            PAPER => SCISSORS,
+            SCISSORS => ROCK,
+            else => unreachable,
+        };
+    }
+}
+
+pub fn score2(data: []const u8) !i32 {
+    var games = std.mem.split(u8, data, "\n");
+    var totalScore: i32 = 0;
+    while (games.next()) |game| {
+        if (game.len == 0) continue;
+        var parts = std.mem.split(u8, game, " ");
+        var playerA = decode(parts.next().?[0]);
+        var playerB = choosePlay(playerA, parts.next().?[0]);
+        totalScore += playerB;
+
+        // 3 points for a win, 1 for a draw
+        if (playerB == playerA + 1 or playerB == playerA - 2) {
+            totalScore += 6;
+        } else if (playerB == playerA) {
+            totalScore += 3;
+        }
+    }
+
+    return totalScore;
+}
+
+pub fn main() !void {
+    std.debug.print("part 1: {}\n", .{try score(input)});
+    std.debug.print("part 2: {}\n", .{try score2(input)});
 }
 
 // zig test a.zig
-test "sample" {
+test "part 1" {
     var result = try score(
         \\A Y
         \\B X
         \\C Z
     );
     try testing.expect(result == 15);
+}
+
+test "part 2" {
+    var result = try score2(
+        \\A Y
+        \\B X
+        \\C Z
+    );
+    try testing.expect(result == 12);
 }
