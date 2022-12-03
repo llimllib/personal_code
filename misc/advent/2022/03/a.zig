d1fc4424e4861729731f2bb6a874ad0ddb897961
Bill Mill <bill@billmill.org>
2022-12-03 10:02:28 -0500

    part 2

--- misc/advent/2022/03/a.zig
+++ misc/advent/2022/03/a.zig
@@ -30,8 +30,43 @@ pub fn score(data: []const u8) !usize {
     return sum;
 }
 
+pub fn score2(data: []const u8) !usize {
+    var packs = std.mem.split(u8, data, "\n");
+    var sum: usize = 0;
+    while (packs.next()) |pack1| {
+        if (pack1.len == 0) continue;
+
+        var pack2 = packs.next().?;
+        var pack3 = packs.next().?;
+        var setA = std.bit_set.IntegerBitSet(64).initEmpty();
+        for (pack1) |char| {
+            setA.set(char - 'A');
+        }
+        var setB = std.bit_set.IntegerBitSet(64).initEmpty();
+        for (pack2) |char| {
+            setB.set(char - 'A');
+        }
+        var setC = std.bit_set.IntegerBitSet(64).initEmpty();
+        for (pack3) |char| {
+            setC.set(char - 'A');
+        }
+        setA.setIntersection(setB);
+        setA.setIntersection(setC);
+
+        var char = setA.findFirstSet().? + 'A';
+        if (char - 'A' < 26) {
+            sum += char - '@' + 26;
+        } else if (char - 'a' < 26) {
+            sum += char - '`';
+        }
+    }
+
+    return sum;
+}
+
 pub fn main() !void {
     std.debug.print("part 1: {}\n", .{try score(input)});
+    std.debug.print("part 2: {}\n", .{try score2(input)});
 }
 
 // zig test a.zig
@@ -46,4 +81,17 @@ test "part 1" {
     );
     std.debug.print("{d}\n", .{result});
     try testing.expect(result == 157);
+}
+
+test "part 2" {
+    var result = try score2(
+        \\vJrwpWtwJgWrhcsFMMfFFhFp
+        \\jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+        \\PmmdzqPrVvPwwTWBwg
+        \\wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+        \\ttgJtRGJQctTZtZT
+        \\CrZsJsPPZsGzwwsLwLmpwMDw
+    );
+    std.debug.print("{d}\n", .{result});
+    try testing.expect(result == 70);
 }
