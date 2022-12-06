a0520ded27cb57b6f81d0d347a3edb4637077c94
Bill Mill <bill@billmill.org>
2022-12-06 09:09:20 -0500

    unify

--- misc/advent/2022/06/a.zig
+++ misc/advent/2022/06/a.zig
@@ -9,48 +9,35 @@ const input = @embedFile("input.txt");
 
 const Charset = std.bit_set.IntegerBitSet(64);
 
-pub fn find4th(data: []const u8) usize {
+pub fn findnth(data: []const u8, n: usize) usize {
     for (data) |_, i| {
         var set = Charset.initEmpty();
-        for (data[i .. i + 4]) |cc| {
+        for (data[i .. i + n]) |cc| {
             set.set(cc - 'a');
         }
-        if (set.count() == 4) {
-            return i + 4;
-        }
-    }
-    unreachable;
-}
-
-pub fn find14th(data: []const u8) usize {
-    for (data) |_, i| {
-        var set = Charset.initEmpty();
-        for (data[i .. i + 14]) |cc| {
-            set.set(cc - 'a');
-        }
-        if (set.count() == 14) {
-            return i + 14;
+        if (set.count() == n) {
+            return i + n;
         }
     }
     unreachable;
 }
 
 pub fn main() !void {
-    std.debug.print("part 1: {}\n", .{find4th(input)});
-    std.debug.print("part 2: {}\n", .{find14th(input)});
+    std.debug.print("part 1: {}\n", .{findnth(input, 4)});
+    std.debug.print("part 2: {}\n", .{findnth(input, 14)});
 }
 
 // zig test a.zig
 test "part 1" {
-    try testing.expect(find4th("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5);
-    try testing.expect(find4th("nppdvjthqldpwncqszvftbrmjlhg") == 6);
-    try testing.expect(find4th("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10);
-    try testing.expect(find4th("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11);
+    try testing.expect(findnth("bvwbjplbgvbhsrlpgdmjqwftvncz", 4) == 5);
+    try testing.expect(findnth("nppdvjthqldpwncqszvftbrmjlhg", 4) == 6);
+    try testing.expect(findnth("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4) == 10);
+    try testing.expect(findnth("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4) == 11);
 }
 
 test "part 2" {
-    try testing.expect(find14th("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 19);
-    try testing.expect(find14th("bvwbjplbgvbhsrlpgdmjqwftvncz") == 23);
-    try testing.expect(find14th("nppdvjthqldpwncqszvftbrmjlhg") == 23);
-    try testing.expect(find14th("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29);
+    try testing.expect(findnth("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14) == 19);
+    try testing.expect(findnth("bvwbjplbgvbhsrlpgdmjqwftvncz", 14) == 23);
+    try testing.expect(findnth("nppdvjthqldpwncqszvftbrmjlhg", 14) == 23);
+    try testing.expect(findnth("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14) == 29);
 }
