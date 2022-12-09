6577ed3b01b9c6f65f1df8f477889abf92fb8e2e
Bill Mill <bill@billmill.org>
2022-12-09 10:04:34 -0500

    simplify

--- misc/advent/2022/09/a.py
+++ misc/advent/2022/09/a.py
@@ -6,9 +6,7 @@ moves = {
 }
 
 
-def move(h, t, d):
-    dx, dy = d
-    h = (h[0] + dx, h[1] + dy)
+def adjust(h, t):
     if t[0] == h[0] and abs(t[1] - h[1]) == 2:
         t = (t[0], t[1] + (1 if t[1] < h[1] else -1))
     elif t[1] == h[1] and abs(t[0] - h[0]) == 2:
@@ -16,7 +14,7 @@ def move(h, t, d):
     elif abs(t[0] - h[0]) + abs(t[1] - h[1]) > 2:
         t = (t[0] + (1 if t[0] < h[0] else -1), t[1] + (1 if t[1] < h[1] else -1))
 
-    return h, t
+    return t
 
 
 def simn(cmds, n_knots):
@@ -26,10 +24,9 @@ def simn(cmds, n_knots):
         dir, n = cmd.split()
         n = int(n)
         for _ in range(n):
-            for i in range(len(knots) - 1):
-                h, t = move(knots[i], knots[i + 1], moves[dir] if i == 0 else (0, 0))
-                knots[i] = h
-                knots[i + 1] = t
+            knots[0] = (knots[0][0] + moves[dir][0], knots[0][1] + moves[dir][1])
+            for i in range(1, len(knots)):
+                knots[i] = adjust(knots[i - 1], knots[i])
             visited.add(knots[-1])
     return len(visited)
 
@@ -42,7 +39,7 @@ R 4
 D 1
 L 5
 R 2"""
-assert simn(sample.split("\n"), 2) == 13
+assert simn(sample.split("\n"), 2) == 13, simn(sample.split("\n"), 2)
 print(simn(open("input.txt").read().strip().split("\n"), 2))
 
 sample2 = """R 5
