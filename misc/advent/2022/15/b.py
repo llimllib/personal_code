bd4fbf57a9bfcfd96c1571491c560392cba10baf
Bill Mill <bill@billmill.org>
2022-12-15 15:23:11 -0500

    cleanup

--- misc/advent/2022/15/b.py
+++ misc/advent/2022/15/b.py
@@ -42,6 +42,13 @@ def points(items, row):
     return merged
 
 
+def findgap(items, max_):
+    for i in range(max_):
+        pts = points(items, i)
+        if len(pts) > 1:
+            return ((pts[0][1] + 1) * 4000000) + i
+
+
 sample = parse(
     """Sensor at x=2, y=18: closest beacon is at x=-2, y=15
 Sensor at x=9, y=16: closest beacon is at x=10, y=16
@@ -58,23 +65,9 @@ Sensor at x=16, y=7: closest beacon is at x=15, y=3
 Sensor at x=14, y=3: closest beacon is at x=15, y=3
 Sensor at x=20, y=1: closest beacon is at x=15, y=3"""
 )
-# assert len(points(sample, 10)) == 26, points(sample, 10)
+
+
+assert findgap(sample, 21) == 56000011
 
 items = parse(open("input.txt").read().strip())
-
-# print("part 1:", len(points(items, 2000000)))
-#
-# assert search(sample, (0, 0, 20, 20)) == 56000011
-#
-# print("part 2:", search(items, (0, 0, 4000000, 4000000)))
-
-for i in range(21):
-    pts = points(sample, i)
-    if len(pts) > 1:
-        print(i, pts)
-
-print("----------")
-for i in range(4000000):
-    pts = points(items, i)
-    if len(pts) > 1:
-        print(i, pts, ((pts[0][1] + 1) * 4000000) + i)
+print("part 2:", findgap(items, 4000000))
