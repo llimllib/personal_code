e44b25bf29cdf93938ac6ae1afd85d4386c352a3
Bill Mill <bill@billmill.org>
2022-12-18 11:01:01 -0500

    visualization

---
+++ misc/advent/2022/18/mpl_voxel.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def parse(text):
+    space = np.zeros((22, 22, 22), dtype=bool)
+    for a, b, c in (l.split(",") for l in text.strip().split("\n")):
+        space[int(a), int(b), int(c)] = True
+    return space
+
+
+def explode(data):
+    size = np.array(data.shape) * 2
+    data_e = np.zeros(size - 1, dtype=data.dtype)
+    data_e[::2, ::2, ::2] = data
+    return data_e
+
+
+# based on
+# https://matplotlib.org/stable/gallery/mplot3d/voxels_numpy_logo.html#sphx-glr-gallery-mplot3d-voxels-numpy-logo-py
+pts = parse(open("input.txt").read())
+
+ax = plt.figure().add_subplot(projection="3d")
+ax.voxels(pts, facecolors="blue", edgecolors="black")
+ax.set_aspect("equal")
+
+plt.show()
