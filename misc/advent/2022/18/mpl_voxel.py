e6880e9f057586f5f4fae2822e45d3cc97977eb8
Bill Mill <bill@billmill.org>
2022-12-18 11:15:26 -0500

    remove unused

--- misc/advent/2022/18/mpl_voxel.py
+++ misc/advent/2022/18/mpl_voxel.py
@@ -9,13 +9,6 @@ def parse(text):
     return space
 
 
-def explode(data):
-    size = np.array(data.shape) * 2
-    data_e = np.zeros(size - 1, dtype=data.dtype)
-    data_e[::2, ::2, ::2] = data
-    return data_e
-
-
 # based on
 # https://matplotlib.org/stable/gallery/mplot3d/voxels_numpy_logo.html#sphx-glr-gallery-mplot3d-voxels-numpy-logo-py
 pts = parse(open("input.txt").read())
