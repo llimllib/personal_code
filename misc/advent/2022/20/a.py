0c8c69985f60a10f0e3cbf5b920a2a7ab6ccae43
Bill Mill <bill@billmill.org>
2022-12-20 09:37:51 -0500

    cleaner

--- misc/advent/2022/20/a.py
+++ misc/advent/2022/20/a.py
@@ -1,29 +1,25 @@
-from ipdb import set_trace as t
-
-
-def mix(data):
+def mix(data, rounds=1):
     sl = len(data)
-    # bit mask tracking what values have been popped
-    ixs = [0 for _ in data]
-    for i in range(sl):
-        ix = ixs.index(0)
-        x = data.pop(ix)
-        ixs.pop(ix)
-        # print("moving", x, "from", ix, "to", (x + ix) % sl)
-        newix = (x + ix) % (sl - 1)
-        data.insert(newix, x)
-        ixs.insert(newix, 1)
-        ixs[newix] = 1
-        # print(data)
-        # print(ixs)
+    for _ in range(rounds):
+        # bit mask tracking what values have been popped
+        ixs = [0 for _ in data]
+        for _ in range(sl):
+            ix = ixs.index(0)
+            x = data.pop(ix)
+            ixs.pop(ix)
+            newix = (x + ix) % (sl - 1)
+            data.insert(newix, x)
+            ixs.insert(newix, 1)
+            ixs[newix] = 1
 
     ix = data.index(0)
     return data[(ix + 1000) % sl] + data[(ix + 2000) % sl] + data[(ix + 3000) % sl]
 
 
-res = mix([1, 2, -3, 3, -2, 0, 4])
+sample = [1, 2, -3, 3, -2, 0, 4]
+res = mix(sample)
 assert res == 3, res
 
-# -8224 and -2392 are both not right
+# 14526 is the right answer
 data = [int(n) for n in open("input.txt").read().strip().split("\n")]
 print(mix(data))
