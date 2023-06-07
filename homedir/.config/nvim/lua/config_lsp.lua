6a39431a832f8a27c1ce56c6947a3316e8d21849
Bill Mill <bill@billmill.org>
2023-06-07 16:13:40 -0400

    try out vtsls

--- homedir/.config/nvim/lua/config_lsp.lua
+++ homedir/.config/nvim/lua/config_lsp.lua
@@ -4,10 +4,18 @@
 -- brew install efm-langserver shellcheck stylua
 -- go install golang.org/x/tools/gopls@latest
 -- go install mvdan.cc/gofumpt@latest
--- npm install -g typescript typescript-language-server prettier bash-language-server vscode-langservers-extracted
+--
+-- npm install -g typescript typescript-language-server @vtsls/language-server prettier bash-language-server vscode-langservers-extracted
 -- gem install solargraph
 -- pip install pyright black
 -- build zls manually: https://github.com/zigtools/zls/wiki/Downloading-and-Building-ZLS#cloning-with-git
+--
+-- add each npm package to ~/.config/asdf/default-npm-packages so they get
+-- installed in each npm version globally; see bashrc for where I set that
+-- filename, it doesn't check XDG dirs by default
+--
+-- same for python, ~/.config/asdf/default-python-packages
+
 local lsp = require("lspconfig")
 local cmp = require("cmp")
 
@@ -165,7 +173,27 @@ lsp.tsserver.setup({
 	settings = { documentFormatting = false },
 	capabilities = capabilities,
 })
--- lsp.flow.setup{ on_attach=on_attach }
+
+-- https://github.com/yioneko/vtsls
+-- npm install -g @vtsls/language-server
+lsp.vtsls.setup({
+	on_attach = function(client, bufnr)
+		-- don't format files, I prefer using prettier
+		client.server_capabilities.document_formatting = false
+
+		-- lsp sets formatexpr here even though I tell it not to use prettier
+		-- for document formatting. Unset it so that `gq` works.
+		--
+		-- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1131
+		vim.api.nvim_buf_set_option(bufnr, "formatexpr", "")
+
+		on_attach(client, bufnr)
+	end,
+	-- don't format files, I prefer using prettier (I'm not even sure that's a
+	-- thing vtsls does?)
+	settings = { documentFormatting = false },
+	capabilities = capabilities,
+})
 
 -- go install golang.org/x/tools/gopls@latest
 lsp.gopls.setup({
