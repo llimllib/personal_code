6da41254d1199edba296dd61926d88cbdef292aa
Bill Mill <bill@billmill.org>
2023-07-31 20:24:45 -0400

    add venv-selector config

--- homedir/.config/LazyVim_starter/lua/plugins/example.lua
+++ homedir/.config/LazyVim_starter/lua/plugins/example.lua
@@ -303,4 +303,13 @@ return {
       timeout = 2000,
     },
   },
+
+  {
+    "linux-cultist/venv-selector.nvim",
+    cmd = "VenvSelect",
+    opts = {
+      name = { "venv", ".venv" },
+    },
+    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
+  },
 }
