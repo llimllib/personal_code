f04b5654f20d7d96872e842c513eebbee0438b34
Bill Mill <bill@billmill.org>
2023-10-19 13:25:20 -0400

    add -h and -v to sync.bash

--- homedir/sync.bash
+++ homedir/sync.bash
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-git pull
+function usage {
+    cat <<EOF
+sync.bash [-v]
+
+sync my home directory to git in the way I prefer
+EOF
+    kill -INT $$
+}
 
 # adapted from
 # https://github.com/dylanaraps/neofetch/blob/master/neofetch#L577
@@ -25,52 +32,73 @@ function ensuredir {
     fi
 }
 
-# TODO: add some sort of ignore file for files I don't want to sync on this
-# machine for w/e reason
-#
-# find all files, except this file
-for f in $(fd --exclude "$(basename "$0")" --type file --hidden .); do
-    src="$f"
-    dest="$HOME/$f"
-    # --exit-code: Make the program exit with codes similar to diff(1). That
-    #              is, it exits with 1 if there were differences and 0 means no
-    #              differences.
-    if ! git diff --exit-code --quiet "$src" "$dest" &> /dev/null; then
-        # if the destination file exists, show a diff and ask the user if they
-        # want to accept the change from either side, or ignore it
-        if [ -e "$dest" ]; then
-            git diff "$dest" "$src" 
-            read -r -p "Take ${red}[h]${fg}omedir version, ${green}[r]${fg}emote version, or ${blue}[i]${fg}gnore: " action
-            case $action in
-                h)
-                    ensuredir "$dest"
-                    rsync "$dest" "$src";;
-                r)
-                    ensuredir "$dest"
-                    rsync "$src" "$dest";;
-            esac
-        # if the destination file doesn't exist, ask if they want to add it
-        else
-            read -r -p "Do you want to ${red}[a]${fg}dd $src to $dest? " action
-            case $action in
-                a)
-                    ensuredir "$dest"
-                    rsync "$src" "$dest";;
-            esac
-        fi
-    fi
-done
+function main {
+    git pull
 
-dirs_to_sync=(.local/bin)
-for dir in "${dirs_to_sync[@]}"; do
-    for f in $(cd "$HOME" && fd --type file --hidden . "$dir"); do
-        if [ ! -f "$f" ]; then
-            read -r -p "Do you want to ${red}[a]${fg}dd $f to repo? " action
-            case $action in
-                a)
-                    ensuredir "$dir"
-                    rsync "$HOME/$src" "./$src";;
-            esac
+    # TODO: add some sort of ignore file for files I don't want to sync on this
+    # machine for w/e reason
+    #
+    # find all files, except this file
+    for f in $(fd --exclude "$(basename "$0")" --type file --hidden .); do
+        src="$f"
+        dest="$HOME/$f"
+        # --exit-code: Make the program exit with codes similar to diff(1). That
+        #              is, it exits with 1 if there were differences and 0 means no
+        #              differences.
+        if ! git diff --exit-code --quiet "$src" "$dest" &> /dev/null; then
+            # if the destination file exists, show a diff and ask the user if they
+            # want to accept the change from either side, or ignore it
+            if [ -e "$dest" ]; then
+                git diff "$dest" "$src" 
+                read -r -p "Take ${red}[h]${fg}omedir version, ${green}[r]${fg}emote version, or ${blue}[i]${fg}gnore: " action
+                case $action in
+                    h)
+                        ensuredir "$dest"
+                        rsync "$dest" "$src";;
+                    r)
+                        ensuredir "$dest"
+                        rsync "$src" "$dest";;
+                esac
+            # if the destination file doesn't exist, ask if they want to add it
+            else
+                read -r -p "Do you want to ${red}[a]${fg}dd $src to $dest? " action
+                case $action in
+                    a)
+                        ensuredir "$dest"
+                        rsync "$src" "$dest";;
+                esac
+            fi
         fi
     done
+
+    dirs_to_sync=(.local/bin)
+    for dir in "${dirs_to_sync[@]}"; do
+        for f in $(cd "$HOME" && fd --type file --hidden . "$dir"); do
+            if [ ! -f "$f" ]; then
+                read -r -p "Do you want to ${red}[a]${fg}dd $f to repo? " action
+                case $action in
+                    a)
+                        ensuredir "$dir"
+                        rsync "$HOME/$src" "./$src";;
+                esac
+            fi
+        done
+    done
+}
+
+while true; do
+    case $1 in
+        help | -h | --help)
+            usage
+            ;;
+        -v | --verbose)
+            set -x
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
 done
+
+main "$@"
