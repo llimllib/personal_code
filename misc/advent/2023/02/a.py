21436b6c0f5d6656a519525359fe72ab4a853cf1
Bill Mill <bill@billmill.org>
2023-12-02 09:21:18 -0500

    simplify a bit

--- misc/advent/2023/02/a.py
+++ misc/advent/2023/02/a.py
@@ -35,35 +35,14 @@ def powersum(games, limit=(12, 13, 14)):
     )
 
 
-print(
-    possible(
-        parse(
-            """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+sample = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""".split(
-                "\n"
-            )
-        )
-    )
-)
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
 
+print(possible(parse(sample.split("\n"))))
 print(possible(parse(open("input.txt"))))
 
-
-print(
-    powersum(
-        parse(
-            """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""".split(
-                "\n"
-            )
-        )
-    )
-)
-
+print(powersum(parse(sample.split("\n"))))
 print(powersum(parse(open("input.txt"))))
