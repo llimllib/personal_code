7da47875cf47a193c8e67d4a5431aa5567d163cb
Bill Mill <bill@billmill.org>
2023-12-06 09:42:54 -0500

    simplify part 2 a touch

--- misc/advent/2023/06/a.py
+++ misc/advent/2023/06/a.py
@@ -17,8 +17,6 @@ print(
 print(
     prod(
         sum(1 for p in range(t) if (t - p) * p > d)
-        for t, d in zip(
-            *[[int(x) for x in ["".join(re.findall(r"\d+", line))]] for line in in2]
-        )
+        for t, d in [[int("".join(re.findall(r"\d+", line))) for line in in2]]
     )
 )
