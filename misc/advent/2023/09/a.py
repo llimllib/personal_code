f0c09f2be4c856c04bcf85ed195375fa60b8722f
Bill Mill <bill@billmill.org>
2023-12-09 12:43:48 -0500

    remove unnecessary part 2

--- misc/advent/2023/09/a.py
+++ misc/advent/2023/09/a.py
@@ -10,17 +10,6 @@ def part1(ints):
     return sum([ints[0]] + ls)
 
 
-def part2(ints):
-    ls = []
-    while any(a != b for a, b in itertools.pairwise(ints)):
-        ls.append(ints[0])
-        ints = [b - a for a, b in itertools.pairwise(ints)]
-    cur = ints[0]
-    for b in reversed(ls):
-        cur = b - cur
-    return cur
-
-
 in1, in2 = itertools.tee(sys.stdin)
 print(sum(part1([int(x) for x in line.split()]) for line in in1))
-print(sum(part2([int(x) for x in line.split()]) for line in in2))
+print(sum(part1(list(reversed([int(x) for x in line.split()]))) for line in in2))
