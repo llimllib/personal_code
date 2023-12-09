96fad384db5eef38375e3336f2a6bda033438c3c
Bill Mill <bill@billmill.org>
2023-12-09 10:25:06 -0500

    day 9

---
+++ misc/advent/2023/09/a.py
@@ -0,0 +1,26 @@
+import itertools
+import sys
+
+
+def part1(ints):
+    ls = []
+    while any(a != b for a, b in itertools.pairwise(ints)):
+        ls += [ints[-1]]
+        ints = [b - a for a, b in itertools.pairwise(ints)]
+    return sum([ints[0]] + ls)
+
+
+def part2(ints):
+    ls = []
+    while any(a != b for a, b in itertools.pairwise(ints)):
+        ls += [ints[0]]
+        ints = [b - a for a, b in itertools.pairwise(ints)]
+    cur = ints[0]
+    for b in reversed(ls):
+        cur = b - cur
+    return cur
+
+
+in1, in2 = itertools.tee(sys.stdin)
+print(sum(part1([int(x) for x in line.split()]) for line in in1))
+print(sum(part2([int(x) for x in line.split()]) for line in in2))
