a976b82536c134c50b28cbdd0dd535146350cb30
Bill Mill <bill@billmill.org>
2023-12-11 22:35:23 -0500

    golfing some more

--- misc/advent/2023/11/b.py
+++ misc/advent/2023/11/b.py
@@ -1,35 +1,28 @@
 import itertools
 import sys
 
-
-def parse(iter):
-    pts = set()
-    for row, line in enumerate(iter):
-        for col, c in enumerate(line.strip()):
-            if c != ".":
-                pts.add((row, col))
-    return pts
-
-
-pts = sorted(parse(sys.stdin))
-rows = {p[0] for p in pts}
-cols = {p[1] for p in pts}
+pts = [
+    (row, col)
+    for row, line in enumerate(sys.stdin)
+    for col, c in enumerate(line.strip())
+    if c != "."
+]
 
 print(
     sum(
         abs(x1 - x2)
-        + len(set(range(min(x1, x2) + 1, max(x1, x2))) - rows)
+        + len(set(range(min(x1, x2) + 1, max(x1, x2))) - {p[0] for p in pts})
         + abs(y1 - y2)
-        + len(set(range(min(y1, y2) + 1, max(y1, y2))) - cols)
+        + len(set(range(min(y1, y2) + 1, max(y1, y2))) - {p[1] for p in pts})
         for (x1, y1), (x2, y2) in itertools.combinations(pts, 2)
     )
 )
 print(
     sum(
         abs(x1 - x2)
-        + len(set(range(min(x1, x2) + 1, max(x1, x2))) - rows) * 999_999
+        + len(set(range(min(x1, x2) + 1, max(x1, x2))) - {p[0] for p in pts}) * 999_999
         + abs(y1 - y2)
-        + len(set(range(min(y1, y2) + 1, max(y1, y2))) - cols) * 999_999
+        + len(set(range(min(y1, y2) + 1, max(y1, y2))) - {p[1] for p in pts}) * 999_999
         for (x1, y1), (x2, y2) in itertools.combinations(pts, 2)
     )
 )
