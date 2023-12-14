b74e58cedf19c7197af102a22bc1637b4136f74e
Bill Mill <bill@billmill.org>
2023-12-14 09:37:19 -0500

    part 1

---
+++ misc/advent/2023/14/a.py
@@ -0,0 +1,28 @@
+import re
+import sys
+
+rocks, pins = [], []
+maxrow = 0
+for row, line in enumerate(sys.stdin):
+    rocks += [[n.start(), row] for n in re.finditer("O", line)]
+    pins += [[n.start(), row] for n in re.finditer("#", line)]
+    maxrow = row + 1
+
+for i, (col, row) in enumerate(rocks):
+    rocks[i][1] = (
+        max(
+            [r for c, r in rocks if r < row and c == col]
+            + [r for c, r in pins if r < row and c == col]
+            + [-1]
+        )
+        + 1
+    )
+
+for row in range(max(r for _, r in rocks + pins) + 1):
+    for col in range(max(c for c, _ in rocks + pins) + 1):
+        sys.stdout.write(
+            "O" if [col, row] in rocks else "#" if [col, row] in pins else "."
+        )
+    sys.stdout.write("\n")
+
+print(sum(maxrow - r for _, r in rocks))
