31169dc595d0ce67e3717e3c7f04ca31195e18cd
Bill Mill <bill@billmill.org>
2023-12-21 08:28:51 -0500

    simpler

--- misc/advent/2023/21/a.py
+++ misc/advent/2023/21/a.py
@@ -10,12 +10,11 @@ for row, line in enumerate(sys.stdin):
         if c == "S":
             gnomes.add(complex(col, row))
 
-N, E, S, W = 0 - 1j, 1 + 0j, 0 + 1j, -1 + 0j
-adj = defaultdict(list)
-for point in garden:
-    for delta in [N, E, S, W]:
-        if point + delta in garden:
-            adj[point].append(point + delta)
+N, E, S, W = -1j, 1 + 0j, 1j, -1 + 0j
+adj = {
+    point: [point + delta for delta in [N, E, S, W] if point + delta in garden]
+    for point in garden
+}
 
 for i in range(64):
     gs = set()
