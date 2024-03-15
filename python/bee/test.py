facc03257035ecf45142b667ed4acde9f5a71888
Bill Mill <bill@billmill.org>
2024-03-15 15:30:00 -0400

    better presentation of results

--- python/bee/test.py
+++ python/bee/test.py
@@ -6,3 +6,12 @@ for word in set(line.strip() for line in open("words.txt") if len(line.strip()) 
         n += 1
         print(word)
 print(n)
+
+
+red = "\033[0;31m"
+green = "\033[0;32m"
+blue = "\033[0;34m"
+reset = "\033[0m"
+print(f"{green}words\tpangram")
+for n, pangram in [(1, "bananas"), (2, "test")]:
+    print(f"{blue}{n}{reset}\t{pangram}")
--- python/bee/worst.py
+++ python/bee/worst.py
@@ -1,10 +1,11 @@
 from collections import Counter
 from itertools import combinations
-from pprint import pprint
 
-allwords = set(line.strip() for line in open("words.txt"))
+# the spelling bee never includes the letter "s"
+alphabet = frozenset("abcdefghijklmnopqrtuvwxyz")
+allwords = set(line.strip() for line in open("words.txt") if "s" not in line)
+
 score = Counter()
-alphabet = frozenset("abcdefghijklmnopqrstuvwxyz")
 for word in allwords:
     ws = frozenset(word)
     if 3 < len(word) and len(ws) < 7:
@@ -14,7 +15,24 @@ for word in allwords:
 pangrams = [
     (score[frozenset(line.strip())] + 1, line.strip())
     for line in open("words.txt")
-    if len(set(line.strip())) == 7
+    if "s" not in line and len(set(line.strip())) == 7
 ]
 pangrams.sort()
-pprint(pangrams[:15])
+
+red = "\033[0;31m"
+green = "\033[0;32m"
+blue = "\033[0;34m"
+reset = "\033[0m"
+print(f"{green}words\tpangram")
+for n, pangram in pangrams[:15]:
+    ps = set(pangram)
+    matches = ",".join(
+        [
+            w
+            for w in [l.strip() for l in open("words.txt")]
+            if set(w).issubset(ps) and len(w) > 3
+        ]
+    )
+    if len(matches) > 60:
+        matches = matches[:59] + "..."
+    print(f"{blue}{n: <8}{red}{pangram: <15}{reset}{matches}")
