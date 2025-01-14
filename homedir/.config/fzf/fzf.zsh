007afc29feeb99a99709603f5b30f06951320e0f
Bill Mill <bill@billmill.org>
2025-01-14 13:33:09 -0500

    chore: add some config dirs

---
+++ homedir/.config/fzf/fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+source "/opt/homebrew/opt/fzf/shell/completion.zsh"
+
+# Key bindings
+# ------------
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
