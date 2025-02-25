955a1beeaeb06e1d260f1b6a0420f4dcb69fde27
Bill Mill <bill@billmill.org>
2025-02-25 09:17:03 -0500

    feat(vim): use claude-3-7 by default

--- homedir/.config/nvim/lua/config_codecompanion.lua
+++ homedir/.config/nvim/lua/config_codecompanion.lua
@@ -18,6 +18,11 @@ require("codecompanion").setup({
 					-- $ security find-generic-password -s 'anthropic' -a 'llimllib' -w
 					api_key = "cmd:security find-generic-password -w -s 'anthropic' | tr -d '\n'",
 				},
+				schema = {
+					model = {
+						default = "claude-3-7-sonnet-20250219",
+					},
+				},
 			})
 		end,
 	},
