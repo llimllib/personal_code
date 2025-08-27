1145b4a3387c1b469ebe434084f7521f5bc5ad69
Bill Mill <bill@billmill.org>
2025-08-27 14:55:20 -0400

    feat(colors): update colorscheme
    this is provisional, trying out some oklch stuff

--- homedir/.config/nvim/lua/colorscheme.lua
+++ homedir/.config/nvim/lua/colorscheme.lua
@@ -8,24 +8,39 @@ vim.g.sonokai_colors_override = {
 	bg2 = { "#2f3548", "236" },
 	bg3 = { "#343b50", "237" },
 	bg4 = { "#394158", "237" },
-	bg_red = { "#ff6d7e", "203" },
-	bg_green = { "#3a8168", "107" },
-	bg_blue = { "#7ad5f1", "110" },
-	bg_purple = { "#d9b1a2", "54" },
-	-- In sonokai, these seem to be slightly brighter variants, but I haven't
-	-- bothered
-	filled_red = { "#F47648", "203" },
-	filled_green = { "#40BA93", "107" },
-	filled_blue = { "#73D0FF", "110" },
 	fg = { "#e1e3e4", "250" },
-	red = { "#F47648", "203" },
-	orange = { "#8ED0B2", "215" },
-	yellow = { "#8ED0B2", "179" },
-	green = { "#40BA93", "107" },
-	blue = { "#73D0FF", "110" },
-	purple = { "#fca07f", "176" },
-	grey = { "#828a9a", "246" },
-	grey_dim = { "#5a6477", "240" },
 	none = { "NONE", "NONE" },
+
+	-- oklch(0.71 0.122 21.36) https://oklch.com/#0.71,0.122,21.36,100
+	red = { "#e18665", "203" },
+	purple = { "#e18665", "176" },
+	-- oklch(0.7882 0.062 21.36) https://oklch.com/#0.7882,0.062,21.36,100
+	bg_purple = { "#dfaba9", "54" },
+
+	-- oklch(0.71 0.1625 21.36) https://oklch.com/#0.71,0.1625,21.36,100
+	filled_red = { "#f67274", "203" },
+	bg_red = { "#f67274", "203" },
+
+	-- oklch(0.8427 0.1225 167.95) https://oklch.com/#0.8427,0.1225,167.95,100
+	orange = { "#70e5bc", "215" },
+	yellow = { "#70e5bc", "179" },
+
+	-- oklch(0.71, 0.122, 167.95) https://oklch.com/#0.711,0.1225,167.95,100
+	green = { "#40BA93", "107" },
+	filled_green = { "#40BA93", "107" },
+	-- oklch(0.6059 0.1225 167.95) https://oklch.com/#0.6059,0.1225,167.95,100
+	bg_green = { "#019974", "107" },
+
+	-- oklch(0.7095 0.122 229.35) https://oklch.com/#0.7095,0.122,229.35,100
+	blue = { "#3aafde", "110" },
+	filled_blue = { "#3aafde", "110" },
+	-- oklch(0.8073 0.0966 229.35) https://oklch.com/#0.8073,0.0966,229.35,100
+	bg_blue = { "#7accf2", "110" },
+
+	-- oklch(0.7095 0.0346 229.35) https://oklch.com/#0.7095,0.0346,229.35,100
+	grey = { "#8ca6b3", "246" },
+
+	-- oklch(0.546 0.0346 229.35) https://oklch.com/#0.546,0.0346,229.35,100
+	grey_dim = { "#5c7581", "240" },
 }
 vim.cmd("colorscheme sonokai")
