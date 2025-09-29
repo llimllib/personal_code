6d19a997bc588bfd519c5b3a36d8a21c6c8358a0
Bill Mill <bill@billmill.org>
2025-09-28 21:00:17 -0400

    feat(nvim): update codecompanion config

--- homedir/.config/nvim/lua/config_codecompanion.lua
+++ homedir/.config/nvim/lua/config_codecompanion.lua
@@ -22,22 +22,24 @@ require("codecompanion").setup({
 		},
 	},
 	adapters = {
-		anthropic = function()
-			return require("codecompanion.adapters").extend("anthropic", {
-				env = {
-					-- saved key with:
-					-- $ security add-generic-password -s 'anthropic' -a 'llimllib' -w 'sk-ant-<key-goes-here>'
-					-- retrieve with:
-					-- $ security find-generic-password -s 'anthropic' -a 'llimllib' -w
-					api_key = "cmd:security find-generic-password -w -s 'anthropic' | tr -d '\n'",
-				},
-				schema = {
-					model = {
-						default = "claude-3-7-sonnet-20250219",
+		http = {
+			anthropic = function()
+				return require("codecompanion.adapters").extend("anthropic", {
+					env = {
+						-- saved key with:
+						-- $ security add-generic-password -s 'anthropic' -a 'llimllib' -w 'sk-ant-<key-goes-here>'
+						-- retrieve with:
+						-- $ security find-generic-password -s 'anthropic' -a 'llimllib' -w
+						api_key = "cmd:security find-generic-password -w -s 'anthropic' | tr -d '\n'",
 					},
-				},
-			})
-		end,
+					schema = {
+						model = {
+							default = "claude-3-7-sonnet-20250219",
+						},
+					},
+				})
+			end,
+		},
 	},
 	extensions = {
 		-- https://codecompanion.olimorris.dev/extensions/history.html
--- homedir/.config/nvim/lua/lazyconfig.lua
+++ homedir/.config/nvim/lua/lazyconfig.lua
@@ -139,6 +139,7 @@ require("lazy").setup({
 						"diff",
 						"elixir",
 						"erlang",
+						"gleam",
 						"glsl",
 						"go",
 						"gomod",
@@ -345,16 +346,6 @@ require("lazy").setup({
 				"ravitemer/mcphub.nvim",
 			},
 			config = true,
-		},
-		{
-			"ravitemer/mcphub.nvim",
-			dependencies = {
-				"nvim-lua/plenary.nvim",
-			},
-			build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
-			config = function()
-				require("mcphub").setup()
-			end,
 		},
 	},
 	defaults = {
