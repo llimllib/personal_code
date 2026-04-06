9ebeaf5a88bba04330ee4f4cd6d9ff2e9275b092
Bill Mill <bill@billmill.org>
2026-04-06 08:07:30 -0400

    chore: . -> : fix nvim deprecation

--- homedir/.config/nvim/lua/none_ls.lua
+++ homedir/.config/nvim/lua/none_ls.lua
@@ -23,7 +23,7 @@ null_ls.setup({
 	-- see:
 	-- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
 	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
+		if client:supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				group = augroup,
